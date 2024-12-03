4b2b8a5acb74e0150cf2f7fc4ac130a422445f06
Freddy Hidalgo-Monchez <freddy.hidalgo.m@gmail.com>
2024-12-03 06:24:10 -0800

    refactor: remove topics

--- output_parsers.py
+++ output_parsers.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel, Field
 
 class ConversationStarters(BaseModel):
     language_based: List[str] = Field(description="Conversation starters based on programming languages")
-    topic_based: List[str] = Field(description="Conversation starters based on repository topics")
     general: List[str] = Field(description="General conversation starters about GitHub activity")
 
 conversation_starters_parser = PydanticOutputParser(pydantic_object=ConversationStarters)
